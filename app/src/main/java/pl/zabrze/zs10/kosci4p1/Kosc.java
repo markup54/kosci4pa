77d9464d5e3845c9c351f3eeda79e7165d35ebed
markup54 <marzenakupka@tlen.pl>
2024-09-10 08:33:28 +0200

    kosci blokowanie

--- app/src/main/java/pl/zabrze/zs10/kosci4p1/Kosc.java
+++ app/src/main/java/pl/zabrze/zs10/kosci4p1/Kosc.java
@@ -5,21 +5,42 @@ import java.util.Random;
 public class Kosc {
     private int wartosc;
     private boolean zablokowana;
+    private int idObrazka;
+
+    private int[] idObrazkow = new int[]{
+            R.drawable.kosc1,
+            R.drawable.kosc2,
+            R.drawable.kosc3,
+            R.drawable.kosc4,
+            R.drawable.kosc5,
+            R.drawable.kosc6,
+    };
 
     public Kosc(int wartosc) {
         this.wartosc = wartosc;
         zablokowana = false;
+        idObrazka = idObrazkow[wartosc-1];
+    }
+
+    public int getIdObrazka() {
+        return idObrazka;
+    }
+
+    public Kosc(){
+        zablokowana = false;
+        rzucKoscia();
+        idObrazka = idObrazkow[wartosc-1];
     }
     public void rzucKoscia(){
         if(!zablokowana){
             Random random = new Random();
 
             wartosc = random.nextInt(6)+1;
-
+            idObrazka = idObrazkow[wartosc-1];
         }
     }
     public void zablokujKosc(){
-        zablokowana = true;
+        zablokowana = !zablokowana;
     }
     public int getWartosc() {
         return wartosc;
--- app/src/main/java/pl/zabrze/zs10/kosci4p1/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/kosci4p1/MainActivity.java
@@ -3,7 +3,9 @@ package pl.zabrze.zs10.kosci4p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +14,60 @@ import java.sql.Array;
 public class MainActivity extends AppCompatActivity {
     public Button buttonRzuc;
     public TextView textViewWynik;
-    public Kosc[] kostki = new Kosc[5];
+    public Kosc[] kostki ;
     public ImageView[] obrazyKosci = new ImageView[5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kostki = new Kosc[]{
+                new Kosc(),new Kosc(),new Kosc(),new Kosc(),new Kosc()
+        };
+        obrazyKosci[0] = findViewById(R.id.imageButton1);
+        obrazyKosci[1] = findViewById(R.id.imageButton2);
+        obrazyKosci[2] = findViewById(R.id.imageButton3);
+        obrazyKosci[3] = findViewById(R.id.imageButton4);
+        obrazyKosci[4] = findViewById(R.id.imageButton5);
+        textViewWynik = findViewById(R.id.textView);
+        buttonRzuc = findViewById(R.id.button);
+        buttonRzuc.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int suma =0;
+
+                        for (Kosc k: kostki) {
+                            k.rzucKoscia();
+                            suma = suma + k.getWartosc();
+                        }
+
+
+                        for (int i = 0; i <obrazyKosci.length ; i++) {
+                            obrazyKosci[i].setImageResource(kostki[i].getIdObrazka());
+                        }
+
+                        textViewWynik.setText(Integer.toString(suma));
+                    }
+                }
+        );
+
+        for (int i = 0; i < obrazyKosci.length; i++) {
+            final int indeks = i;
+            obrazyKosci[i].setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            kostki[indeks].zablokujKosc();
+                            if(kostki[indeks].isZablokowana() == true) {
+                                obrazyKosci[indeks].setImageAlpha(50);
+                            }
+                            else{
+                                obrazyKosci[indeks].setImageAlpha(200);
+                            }
+                        }
+                    }
+            );
+        }
     }
 }
